6d59f27566e217d1d0cebaa66ff9e9671082f01b
Connor Van Etten <87097190+Crvanetten15@users.noreply.github.com>
2023-02-24 14:57:25 -0500

    Adding Files to help me learn Cpp

---
+++ Main.cpp
@@ -0,0 +1,10 @@
+// Your First C++ Program
+#include <iostream> // Imports in C++
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!";
+    return 0; // Like C it does not need to show a return in Main but best Practice
+    
+}
